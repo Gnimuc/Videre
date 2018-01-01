6a8f77324e9e33619ebc7c65b11fc8adf39cfd12
Gnimuc Key <GnimucKey@gmail.com>
2018-01-01 15:39:47 +0800

    Add geometry shader example

---
+++ OpenGL 4 Tutorials/22_geom_shaders/geom.jl
@@ -0,0 +1,134 @@
+include(joinpath(@__DIR__, "glutils.jl"))
+
+@static if is_apple()
+    const VERSION_MAJOR = 4
+    const VERSION_MINOR = 1
+end
+
+# window init global variables
+glfwWidth = 640
+glfwHeight = 480
+window = C_NULL
+
+# start OpenGL
+@assert startgl()
+
+glEnable(GL_DEPTH_TEST)
+glDepthFunc(GL_LESS)
+
+# load shaders from file
+const vertexShader = readstring(joinpath(@__DIR__, "geom.vert"))
+const geometryShader = readstring(joinpath(@__DIR__, "geom.geom"))
+const fragmentShader = readstring(joinpath(@__DIR__, "geom.frag"))
+
+# compile shaders and check for shader compile errors
+vertexShaderID = glCreateShader(GL_VERTEX_SHADER)
+glShaderSource(vertexShaderID, 1, Ptr{GLchar}[pointer(vertexShader)], C_NULL)
+glCompileShader(vertexShaderID)
+# get shader compile status
+compileResult = Ref{GLint}(-1)
+glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, compileResult)
+if compileResult[] != GL_TRUE
+    logger = getlogger(current_module())
+    warn(logger, string("GL vertex shader(index", vertexShaderID, ")did not compile."))
+    shaderlog(vertexShaderID)
+    error("GL vertex shader(index ", vertexShaderID, " )did not compile.")
+end
+
+geometryShaderID = glCreateShader(GL_GEOMETRY_SHADER)
+glShaderSource(geometryShaderID, 1, Ptr{GLchar}[pointer(geometryShader)], C_NULL)
+glCompileShader(geometryShaderID)
+# get shader compile status
+compileResult = Ref{GLint}(-1)
+glGetShaderiv(geometryShaderID, GL_COMPILE_STATUS, compileResult)
+if compileResult[] != GL_TRUE
+    logger = getlogger(current_module())
+    warn(logger, string("GL vertex shader(index", geometryShaderID, ")did not compile."))
+    shaderlog(geometryShaderID)
+    error("GL vertex shader(index ", geometryShaderID, " )did not compile.")
+end
+
+fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER)
+glShaderSource(fragmentShaderID, 1, Ptr{GLchar}[pointer(fragmentShader)], C_NULL)
+glCompileShader(fragmentShaderID)
+# checkout shader compile status
+compileResult = Ref{GLint}(-1)
+glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, compileResult)
+if compileResult[] != GL_TRUE
+    logger = getlogger(current_module())
+    warn(logger, string("GL fragment shader(index ", fragmentShaderID, " )did not compile."))
+    shaderlog(fragmentShaderID)
+    error("GL fragment shader(index ", fragmentShaderID, " )did not compile.")
+end
+
+# create and link shader program
+shaderProgramID = glCreateProgram()
+glAttachShader(shaderProgramID, vertexShaderID)
+glAttachShader(shaderProgramID, geometryShaderID)
+glAttachShader(shaderProgramID, fragmentShaderID)
+glLinkProgram(shaderProgramID)
+# checkout programe linking status
+linkingResult = Ref{GLint}(-1)
+glGetProgramiv(shaderProgramID, GL_LINK_STATUS, linkingResult)
+if linkingResult[] != GL_TRUE
+    logger = getlogger(current_module())
+    warn(logger, string("Could not link shader programme GL index: ", shaderProgramID))
+    programlog(shaderProgramID)
+    error("Could not link shader programme GL index: ", shaderProgramID)
+end
+
+# vertex data
+points = GLfloat[ 0.0,  0.5, 0.0,
+                  0.5, -0.5, 0.0,
+                 -0.5, -0.5, 0.0]
+
+colors = GLfloat[ 1.0, 0.0, 0.0,
+                  0.0, 1.0, 0.0,
+                  0.0, 0.0, 1.0]
+
+# create buffers located in the memory of graphic card
+pointsVBO = Ref{GLuint}(0)
+glGenBuffers(1, pointsVBO)
+glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[])
+glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW)
+
+colorsVBO = Ref{GLuint}(0)
+glGenBuffers(1, colorsVBO)
+glBindBuffer(GL_ARRAY_BUFFER, colorsVBO[])
+glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW)
+
+# create VAO
+vaoID = Ref{GLuint}(0)
+glGenVertexArrays(1, vaoID)
+glBindVertexArray(vaoID[])
+glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[])
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, C_NULL)
+glBindBuffer(GL_ARRAY_BUFFER, colorsVBO[])
+glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, C_NULL)
+glEnableVertexAttribArray(0)
+glEnableVertexAttribArray(1)
+
+# verbose infos
+printall(shaderProgramID)
+@assert validprogram(shaderProgramID)
+
+# set background color to gray
+glClearColor(0.2, 0.2, 0.2, 1.0)
+
+# render
+while !GLFW.WindowShouldClose(window)
+    updatefps(window)
+    # clear drawing surface
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glViewport(0, 0, glfwWidth, glfwHeight)
+    # drawing
+    glUseProgram(shaderProgramID)
+    glBindVertexArray(vaoID[])
+    glDrawArrays(GL_POINTS, 0, 3)
+    # check and call events
+    GLFW.PollEvents()
+    # swap the buffers
+    GLFW.SwapBuffers(window)
+end
+
+GLFW.DestroyWindow(window)
