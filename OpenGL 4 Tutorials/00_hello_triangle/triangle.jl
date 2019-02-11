7a02b6332da10ec6f91b0d64cb6d166f7311e7a7
Gnimuc <qiyupei@gmail.com>
2019-02-11 10:06:58 +0800

    Update triangle.jl

--- OpenGL 4 Tutorials/00_hello_triangle/triangle.jl
+++ OpenGL 4 Tutorials/00_hello_triangle/triangle.jl
@@ -1,5 +1,6 @@
 using GLFW
 using ModernGL
+using CSyntax
 
 # set up OpenGL context version
 # it seems OpenGL 4.1 is the highest version supported by MacOS.
@@ -33,14 +34,14 @@ glEnable(GL_DEPTH_TEST)
 glDepthFunc(GL_LESS)
 
 # hard-coded shaders
-const vertexShader = """
+const vertex_shader = """
 #version 410 core
 in vec3 vp;
 void main(void)
 {
     gl_Position = vec4(vp, 1.0);
 }"""
-const fragmentShader = """
+const fragment_shader = """
 #version 410 core
 out vec4 frag_colour;
 void main(void)
@@ -49,18 +50,18 @@ void main(void)
 }"""
 
 # compile shaders
-vertexShaderID = glCreateShader(GL_VERTEX_SHADER)
-glShaderSource(vertexShaderID, 1, Ptr{GLchar}[pointer(vertexShader)], C_NULL)
-glCompileShader(vertexShaderID)
-fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER)
-glShaderSource(fragmentShaderID, 1, Ptr{GLchar}[pointer(fragmentShader)], C_NULL)
-glCompileShader(fragmentShaderID)
+vertex_shader_id = glCreateShader(GL_VERTEX_SHADER)
+glShaderSource(vertex_shader_id, 1, Ptr{GLchar}[pointer(vertex_shader)], C_NULL)
+glCompileShader(vertex_shader_id)
+fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER)
+glShaderSource(fragment_shader_id, 1, Ptr{GLchar}[pointer(fragment_shader)], C_NULL)
+glCompileShader(fragment_shader_id)
 
 # create and link shader program
-shaderProgram = glCreateProgram()
-glAttachShader(shaderProgram, vertexShaderID)
-glAttachShader(shaderProgram, fragmentShaderID)
-glLinkProgram(shaderProgram)
+program_id = glCreateProgram()
+glAttachShader(program_id, vertex_shader_id)
+glAttachShader(program_id, fragment_shader_id)
+glLinkProgram(program_id)
 
 # vertex data
 points = GLfloat[ 0.0,  0.5, 0.0,
@@ -68,16 +69,16 @@ points = GLfloat[ 0.0,  0.5, 0.0,
                  -0.5, -0.5, 0.0]
 
 # create buffers located in the memory of graphic card
-vboID = Ref{GLuint}(0)
-glGenBuffers(1, vboID)
-glBindBuffer(GL_ARRAY_BUFFER, vboID[])
+vbo = GLuint(0)
+@c glGenBuffers(1, &vbo)
+glBindBuffer(GL_ARRAY_BUFFER, vbo)
 glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW)
 
 # create VAO
-vaoID = Ref{GLuint}(0)
-glGenVertexArrays(1, vaoID)
-glBindVertexArray(vaoID[])
-glBindBuffer(GL_ARRAY_BUFFER, vboID[])
+vao = GLuint(0)
+@c glGenVertexArrays(1, &vao)
+glBindVertexArray(vao)
+glBindBuffer(GL_ARRAY_BUFFER, vbo)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, C_NULL)
 glEnableVertexAttribArray(0)
 
@@ -87,8 +88,8 @@ glClearColor(0.2, 0.2, 0.2, 1.0)
 # render
 while !GLFW.WindowShouldClose(window)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glUseProgram(shaderProgram)
-    glBindVertexArray(vaoID[])
+    glUseProgram(program_id)
+    glBindVertexArray(vao)
     glDrawArrays(GL_TRIANGLES, 0, 3)
     # check and call events
     GLFW.PollEvents()
