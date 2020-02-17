9dd0ebb5e872977949135a189c0538e37dae8144
Gnimuc <qiyupei@gmail.com>
2020-02-17 09:43:44 +0800

    Fix geom shaders example

--- OpenGL 4 Tutorials/22_geom_shaders/geom.jl
+++ OpenGL 4 Tutorials/22_geom_shaders/geom.jl
@@ -1,81 +1,18 @@
-include(joinpath(@__DIR__, "glutils.jl"))
+using CSyntax
 
 @static if Sys.isapple()
     const VERSION_MAJOR = 4
     const VERSION_MINOR = 1
 end
 
-# window init global variables
-glfwWidth = 640
-glfwHeight = 480
-window = C_NULL
+include(joinpath(@__DIR__, "glutils.jl"))
 
-# start OpenGL
-@assert startgl()
+# init window
+width, height = fb_width, fb_height = 640, 480
+window = startgl(width, height)
 
 glEnable(GL_DEPTH_TEST)
 glDepthFunc(GL_LESS)
-
-# load shaders from file
-const vertexShader = readstring(joinpath(@__DIR__, "geom.vert"))
-const geometryShader = readstring(joinpath(@__DIR__, "geom.geom"))
-const fragmentShader = readstring(joinpath(@__DIR__, "geom.frag"))
-
-# compile shaders and check for shader compile errors
-vertexShaderID = glCreateShader(GL_VERTEX_SHADER)
-glShaderSource(vertexShaderID, 1, Ptr{GLchar}[pointer(vertexShader)], C_NULL)
-glCompileShader(vertexShaderID)
-# get shader compile status
-compileResult = Ref{GLint}(-1)
-glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, compileResult)
-if compileResult[] != GL_TRUE
-    logger = getlogger(current_module())
-    warn(logger, string("GL vertex shader(index", vertexShaderID, ")did not compile."))
-    shaderlog(vertexShaderID)
-    error("GL vertex shader(index ", vertexShaderID, " )did not compile.")
-end
-
-geometryShaderID = glCreateShader(GL_GEOMETRY_SHADER)
-glShaderSource(geometryShaderID, 1, Ptr{GLchar}[pointer(geometryShader)], C_NULL)
-glCompileShader(geometryShaderID)
-# get shader compile status
-compileResult = Ref{GLint}(-1)
-glGetShaderiv(geometryShaderID, GL_COMPILE_STATUS, compileResult)
-if compileResult[] != GL_TRUE
-    logger = getlogger(current_module())
-    warn(logger, string("GL vertex shader(index", geometryShaderID, ")did not compile."))
-    shaderlog(geometryShaderID)
-    error("GL vertex shader(index ", geometryShaderID, " )did not compile.")
-end
-
-fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER)
-glShaderSource(fragmentShaderID, 1, Ptr{GLchar}[pointer(fragmentShader)], C_NULL)
-glCompileShader(fragmentShaderID)
-# checkout shader compile status
-compileResult = Ref{GLint}(-1)
-glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, compileResult)
-if compileResult[] != GL_TRUE
-    logger = getlogger(current_module())
-    warn(logger, string("GL fragment shader(index ", fragmentShaderID, " )did not compile."))
-    shaderlog(fragmentShaderID)
-    error("GL fragment shader(index ", fragmentShaderID, " )did not compile.")
-end
-
-# create and link shader program
-shaderProgramID = glCreateProgram()
-glAttachShader(shaderProgramID, vertexShaderID)
-glAttachShader(shaderProgramID, geometryShaderID)
-glAttachShader(shaderProgramID, fragmentShaderID)
-glLinkProgram(shaderProgramID)
-# checkout programe linking status
-linkingResult = Ref{GLint}(-1)
-glGetProgramiv(shaderProgramID, GL_LINK_STATUS, linkingResult)
-if linkingResult[] != GL_TRUE
-    logger = getlogger(current_module())
-    warn(logger, string("Could not link shader programme GL index: ", shaderProgramID))
-    programlog(shaderProgramID)
-    error("Could not link shader programme GL index: ", shaderProgramID)
-end
 
 # vertex data
 points = GLfloat[ 0.0,  0.5, 0.0,
@@ -87,48 +24,55 @@ colors = GLfloat[ 1.0, 0.0, 0.0,
                   0.0, 0.0, 1.0]
 
 # create buffers located in the memory of graphic card
-pointsVBO = Ref{GLuint}(0)
-glGenBuffers(1, pointsVBO)
-glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[])
+points_vbo = GLuint(0)
+@c glGenBuffers(1, &points_vbo)
+glBindBuffer(GL_ARRAY_BUFFER, points_vbo)
 glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW)
 
-colorsVBO = Ref{GLuint}(0)
-glGenBuffers(1, colorsVBO)
-glBindBuffer(GL_ARRAY_BUFFER, colorsVBO[])
+colors_vbo = GLuint(0)
+@c glGenBuffers(1, &colors_vbo)
+glBindBuffer(GL_ARRAY_BUFFER, colors_vbo)
 glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW)
 
 # create VAO
-vaoID = Ref{GLuint}(0)
-glGenVertexArrays(1, vaoID)
-glBindVertexArray(vaoID[])
-glBindBuffer(GL_ARRAY_BUFFER, pointsVBO[])
+vao = GLuint(0)
+@c glGenVertexArrays(1, &vao)
+glBindVertexArray(vao)
+glBindBuffer(GL_ARRAY_BUFFER, points_vbo)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, C_NULL)
-glBindBuffer(GL_ARRAY_BUFFER, colorsVBO[])
+glBindBuffer(GL_ARRAY_BUFFER, colors_vbo)
 glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, C_NULL)
 glEnableVertexAttribArray(0)
 glEnableVertexAttribArray(1)
 
-# verbose infos
-printall(shaderProgramID)
-@assert validprogram(shaderProgramID)
+# load and compile shaders from file
+vert_shader = createshader(joinpath(@__DIR__, "geom.vert"), GL_VERTEX_SHADER)
+geom_shader = createshader(joinpath(@__DIR__, "geom.geom"), GL_GEOMETRY_SHADER)
+frag_shader = createshader(joinpath(@__DIR__, "geom.frag"), GL_FRAGMENT_SHADER)
+
+# link program
+shader_prog = createprogram(vert_shader, geom_shader, frag_shader)
 
 # set background color to gray
 glClearColor(0.2, 0.2, 0.2, 1.0)
 
-# render
+let
+updatefps = FPSCounter()
+# rendering loop
 while !GLFW.WindowShouldClose(window)
     updatefps(window)
     # clear drawing surface
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glViewport(0, 0, glfwWidth, glfwHeight)
+    glViewport(0, 0, width, height)
     # drawing
-    glUseProgram(shaderProgramID)
-    glBindVertexArray(vaoID[])
+    glUseProgram(shader_prog)
+    glBindVertexArray(vao)
     glDrawArrays(GL_POINTS, 0, 3)
     # check and call events
     GLFW.PollEvents()
     # swap the buffers
     GLFW.SwapBuffers(window)
 end
+end # let
 
 GLFW.DestroyWindow(window)
--- OpenGL 4 Tutorials/22_geom_shaders/glutils.jl
+++ OpenGL 4 Tutorials/22_geom_shaders/glutils.jl
@@ -1,182 +1,177 @@
 using GLFW
 using ModernGL
-using Memento
+using CSyntax
+using Printf
 
-# checkout shader infos
-function shaderlog(shaderID::GLuint)
-    max_length = Ref{GLsizei}(0)
-    glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, max_length)
-    actual_length = Ref{GLsizei}(0)
-    log = Vector{GLchar}(max_length[])
-    glGetShaderInfoLog(shaderID, max_length[], actual_length, log)
-    logger = getlogger(current_module())
-    info(logger, string("shader info log for GL index ", shaderID, ":"))
-    info(logger, String(log))
+# print errors in shader compilation
+function shader_info_log(shader::GLuint)
+    max_length = GLint(0)
+    @c glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &max_length)
+    actual_length = GLsizei(0)
+    log = Vector{GLchar}(undef, max_length)
+    @c glGetShaderInfoLog(shader, max_length, &actual_length, log)
+    @info "shader info log for GL index $shader: $(String(log))"
 end
 
-# checkout program infos
-function programlog(programID::GLuint)
-    max_length = Ref{GLsizei}(0)
-    glGetProgramiv(programID, GL_INFO_LOG_LENGTH, max_length)
-    actual_length = Ref{GLsizei}(0)
-    log = Vector{GLchar}(max_length[])
-    glGetProgramInfoLog(programID, max_length[], actual_length, log)
-    logger = getlogger(current_module())
-    info(logger, string("program info log for GL index ", programID, ":"))
-    info(logger, String(log))
-end
-
-# print verbose infos
-function printall(shaderProgramID::GLuint)
-    result = Ref{GLint}(-1)
-    logger = getlogger(current_module())
-    info(logger, string("Shader Program ", shaderProgramID, " verbose info:"))
-    glGetProgramiv(shaderProgramID, GL_LINK_STATUS, result)
-    info(logger, string("GL_LINK_STATUS = ", result[]))
-
-    glGetProgramiv(shaderProgramID, GL_ATTACHED_SHADERS, result)
-    info(logger, string("GL_ATTACHED_SHADERS = ", result[]))
-
-    glGetProgramiv(shaderProgramID, GL_ACTIVE_ATTRIBUTES, result)
-    info(logger, string("GL_ACTIVE_ATTRIBUTES = ", result[]))
-
-    for i in 1:result[]
-        max_length = Ref{GLsizei}(0)
-        glGetProgramiv(shaderProgramID, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, max_length)
-        actual_length = Ref{GLsizei}(0)
-        attributeSize = Ref{GLint}(0)
-        attributeType = Ref{GLenum}(0)
-        name = Vector{GLchar}(max_length[])
-        glGetActiveAttrib(shaderProgramID, i-1, max_length[], actual_length, attributeSize, attributeType, name)
-        if attributeSize[] > 1
-            for j = 1:attributeSize[]
-                longName = @sprintf "%s[%i]" name j
-                location = glGetAttribLocation(shaderProgramID, longName)
-                log = string("  ", i, ") type:", GLENUM(attributeType[]).name, " name:", String(longName), " location:", location)
-                info(logger, log)
-            end
-        else
-            location = glGetAttribLocation(shaderProgramID, name)
-            log = string("  ", i, ") type:", GLENUM(attributeType[]).name, " name:", String(name), " location:", location)
-            info(logger, log)
-        end
-    end
-
-    glGetProgramiv(shaderProgramID, GL_ACTIVE_UNIFORMS, result)
-    info(logger, string("GL_ACTIVE_UNIFORMS = ", result[]))
-    for i in 1:result[]
-        max_length = Ref{GLsizei}(0)
-        glGetProgramiv(shaderProgramID, GL_ACTIVE_UNIFORM_MAX_LENGTH, max_length)
-        actual_length = Ref{GLsizei}(0)
-        attributeSize = Ref{GLint}(0)
-        attributeType = Ref{GLenum}(0)
-        name = Vector{GLchar}(max_length[])
-        glGetActiveUniform(shaderProgramID, i-1, max_length[], actual_length, attributeSize, attributeType, name)
-        if attributeSize[] > 1
-            for j = 1:attributeSize[]
-                longName = @sprintf "%s[%i]" name j
-                location = glGetUniformLocation(shaderProgramID, longName)
-                log = string("  ", i, ") type:", GLENUM(attributeType[]).name, " name:", String(longName), " location:", location)
-                info(logger, log)
-            end
-        else
-            location = glGetUniformLocation(shaderProgramID, name)
-            log = string("  ", i, ") type:", GLENUM(attributeType[]).name, " name:", String(name), " location:", location)
-            info(logger, log)
-        end
-    end
-    programlog(shaderProgramID)
+# print errors in shader linking
+function programme_info_log(program::GLuint)
+    max_length = GLint(0)
+    @c glGetShaderiv(program, GL_INFO_LOG_LENGTH, &max_length)
+    actual_length = GLsizei(0)
+    log = Vector{GLchar}(undef, max_length)
+    @c glGetShaderInfoLog(program, max_length, &actual_length, log)
+    @info "program info log for GL index $program: $(String(log))"
 end
 
 # validate shader program
-function validprogram(shaderProgramID::GLuint)
-    validResult = Ref{GLint}(-1)
-    glValidateProgram(shaderProgramID)
-    glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, validResult)
-    log = string("program ", shaderProgramID, " GL_VALIDATE_STATUS = ", validResult[])
-    logger = getlogger(current_module())
-    info(logger, log)
-    if validResult[] != GL_TRUE
-        programlog(shaderProgramID)
-        return false
-    end
-    return true
+function is_valid(program::GLuint)
+    params = GLint(-1)
+    glValidateProgram(program)
+    @c glGetProgramiv(program, GL_VALIDATE_STATUS, &params)
+    @info "program $program GL_VALIDATE_STATUS = $params"
+    params == GL_TRUE && return true
+    programme_info_log(program)
+    return false
 end
 
-# load OpenGL parameters info
-function glparams()
-    params = GLenum[ GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS,
-                     GL_MAX_CUBE_MAP_TEXTURE_SIZE,
-                     GL_MAX_DRAW_BUFFERS,
-                     GL_MAX_FRAGMENT_UNIFORM_COMPONENTS,
-                     GL_MAX_TEXTURE_IMAGE_UNITS,
-                     GL_MAX_TEXTURE_SIZE,
-                     GL_MAX_VARYING_FLOATS,
-                     GL_MAX_VERTEX_ATTRIBS,
-                     GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS,
-                     GL_MAX_VERTEX_UNIFORM_COMPONENTS,
-                     GL_MAX_VIEWPORT_DIMS,
-                     GL_STEREO ]
-    names = [ "GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS",
-    	 	  "GL_MAX_CUBE_MAP_TEXTURE_SIZE",
-    		  "GL_MAX_DRAW_BUFFERS",
-    		  "GL_MAX_FRAGMENT_UNIFORM_COMPONENTS",
-        	  "GL_MAX_TEXTURE_IMAGE_UNITS",
-        	  "GL_MAX_TEXTURE_SIZE",
-        	  "GL_MAX_VARYING_FLOATS",
-        	  "GL_MAX_VERTEX_ATTRIBS",
-        	  "GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS",
-        	  "GL_MAX_VERTEX_UNIFORM_COMPONENTS",
-        	  "GL_MAX_VIEWPORT_DIMS",
-        	  "GL_STEREO" ]
+# print verbose infos
+function print_all(shader_prog::GLuint)
+    params = GLint(-1)
 
-    logger = getlogger(current_module())
-    info(logger, "GL Context Params:")
-    for i = 1:10
-        v = Ref{GLint}(0)
-        glGetIntegerv(params[i], v)
-        info(logger, string(names[i], ": ", v[]))
+    @debug "-------------------------"
+    @debug "Shader programme $shader_prog info:"
+    @c glGetProgramiv(shader_prog, GL_LINK_STATUS, &params)
+    @debug "GL_LINK_STATUS = $params"
+
+    @c glGetProgramiv(shader_prog, GL_ATTACHED_SHADERS, &params)
+    @debug "GL_ATTACHED_SHADERS = $params"
+
+    @c glGetProgramiv(shader_prog, GL_ACTIVE_ATTRIBUTES, &params)
+    @debug "GL_ACTIVE_ATTRIBUTES = $params"
+
+    max_length = GLint(0)
+    @c glGetProgramiv(shader_prog, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, &max_length)
+    name = Vector{GLchar}(undef, max_length)
+    for i in 0:params-1
+        actual_length = GLsizei(0)
+        size = GLint(0)
+        type = GLenum(0)
+        @c glGetActiveAttrib(shader_prog, i, max_length, &actual_length, &size, &type, name)
+        if size > 1
+            for j = 0:size-1
+                longname = @sprintf "%s[%i]" name j
+                location = glGetAttribLocation(shader_prog, longname)
+                @debug "  $i): type -> $(GLENUM(type).name), name -> $(String(longname)), location -> $location."
+            end
+        else
+            location = glGetAttribLocation(shader_prog, name)
+            @debug "  $i): type -> $(GLENUM(type).name), name -> $(String(name)), location -> $location."
+        end
     end
-    v = GLint[0, 0]
-    s = Ref{GLboolean}(0)
-    glGetIntegerv(params[11], v)
-    info(logger, string(names[11], ": ", v[1], " | ", v[2]))
-    glGetBooleanv(params[12], s)
-    info(logger, string(names[12], ": ", s[]))
-    info(logger, "-----------------------------")
-    return nothing
+
+    @c glGetProgramiv(shader_prog, GL_ACTIVE_UNIFORMS, &params)
+    @debug "GL_ACTIVE_UNIFORMS = $(paramsRef[])"
+    for i in 0:params-1
+        max_length = GLint(0)
+        @c glGetProgramiv(shader_prog, GL_ACTIVE_UNIFORM_MAX_LENGTH, &max_length)
+        name = Vector{GLchar}(undef, max_length)
+        actual_length = GLsizei(0)
+        size = GLint(0)
+        type = GLenum(0)
+        @c glGetActiveUniform(shader_prog, i, max_length, &actual_length, &size, &type, name)
+        if size > 1
+            for j = 0:size-1
+                longname = @sprintf "%s[%i]" name j
+                location = glGetUniformLocation(shader_prog, longname)
+                @debug "  $i): type -> $(GLENUM(type).name), name -> $(String(longname)), location -> $location."
+            end
+        else
+            location = glGetUniformLocation(shader_prog, name)
+            @debug "  $i): type -> $(GLENUM(type).name), name -> $(String(name)), location -> $location."
+        end
+    end
+
+    programme_info_log(shader_prog)
 end
 
+# create shader
+function createshader(path::AbstractString, type::GLenum)
+    source = read(path, String)
+    id = glCreateShader(type)
+    glShaderSource(id, 1, Ptr{GLchar}[pointer(source)], C_NULL)
+    glCompileShader(id)
+    # get shader compile status and print logs
+    result = GLint(-1)
+    @c glGetShaderiv(id, GL_COMPILE_STATUS, &result)
+    if result != GL_TRUE
+        @error "$(GLENUM(type).name)(id:$id) failed to compile!"
+        max_length = GLint(0)
+        @c glGetShaderiv(id, GL_INFO_LOG_LENGTH, &max_length)
+        actual_length = GLsizei(0)
+        log = Vector{GLchar}(undef, max_length)
+        @c glGetShaderInfoLog(id, max_length, &actual_length, log)
+        @error String(log)
+    end
+    @info "$(GLENUM(type).name)(id:$id) successfully compiled!"
+    return id
+end
+
+# create program
+function createprogram(shaders::GLuint...)
+    id = glCreateProgram()
+    @info "Creating program(id:$id) ..."
+    for shader in shaders
+        @info "  attempting to attach shader(id:$shader) ..."
+        glAttachShader(id, shader)
+    end
+    glLinkProgram(id)
+    # checkout linking status
+    result = GLint(-1)
+    @c glGetProgramiv(id, GL_LINK_STATUS, &result)
+    if result != GL_TRUE
+        @error "Could not link shader program(id:$id)!"
+		max_length = GLint(0)
+        @c glGetProgramiv(id, GL_INFO_LOG_LENGTH, &max_length)
+        actual_length = GLsizei(0)
+        log = Vector{GLchar}(undef, max_length)
+        @c glGetProgramInfoLog(id, max_length, &actual_length, log)
+        @error String(log)
+        error("Could not link shader program(id:$id)!")
+    end
+    @assert is_valid(id)
+	foreach(id->glDeleteShader(id), shaders)
+    return id
+end
 
 ## GLFW initialization
 # set up GLFW key callbacks : press Esc to escape
-function key_callback(window::GLFW.Window, key::Cint, scancode::Cint, action::Cint, mods::Cint)
-    if (key == GLFW.KEY_ESCAPE && action == GLFW.PRESS)
-        GLFW.SetWindowShouldClose(window, true)
-    end
+function key_callback(window::GLFW.Window, key::GLFW.Key, scancode::Cint, action::GLFW.Action, mods::Cint)
+	key == GLFW.KEY_ESCAPE && action == GLFW.PRESS && GLFW.SetWindowShouldClose(window, GL_TRUE)
 end
 
-# : change window size
-function window_size_callback(window::GLFW.Window, width::Cint, height::Cint)
-    global glfwWidth = width
-    global glfwHeight = height
-    println("width", width, "height", height)
-    # update any perspective matrices used here
+# tell GLFW to run this function whenever the framebuffer size is changed
+function framebuffer_size_callback(window::GLFW.Window, w::Cint, h::Cint)
+	global fb_width = w
+	global fb_height = h
+	println("framebuffer size: ($w, $h)")
+    return nothing
+end
+
+# tell GLFW to run this function whenever the window size is changed
+function window_size_callback(window::GLFW.Window, w::Cint, h::Cint)
+	global width = w
+	global height = h
+	println("window size: ($w, $h)")
     return nothing
 end
 
 # error callback
-function error_callback(error::Cint, description::Ptr{GLchar})
-    logger = getlogger(current_module())
-    s = @sprintf "GLFW ERROR: code %i msg: %s" error description
-	error(logger, s)
-    return nothing
-end
-
+error_callback(err::GLFW.GLFWError) = @error "GLFW ERROR: code $(err.code) msg: $(err.description)"
+GLFW.SetErrorCallback(error_callback)
 
 # start OpenGL
-function startgl()
-    @static if Sys.isapple()
+function startgl(width, height)
+	@static if Sys.isapple()
         GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, VERSION_MAJOR)
         GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, VERSION_MINOR)
         GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE)
@@ -185,48 +180,41 @@ function startgl()
         GLFW.DefaultWindowHints()
     end
 
-    # set up GLFW log and error callbacks
-    Memento.config("notice"; fmt="[ {date} | {level} ]: {msg}")
-    logger = getlogger(current_module())
-    push!(logger, DefaultHandler("gl.log", DefaultFormatter("[{date} | {level}]: {msg}")))
-    setlevel!(logger, "info")
-    info(logger, "starting GLFW ...")
-    info(logger, GLFW.GetVersionString())
-    GLFW.SetErrorCallback(error_callback)
+    @info "starting GLFW ..."
+    @info GLFW.GetVersionString()
 
-    # create window
-    global window = GLFW.CreateWindow(glfwWidth, glfwHeight, "Extended Init.")
-    @assert window != C_NULL "could not open window with GLFW3."
+	window = GLFW.CreateWindow(width, height, "Extended Init.")
+	@assert window != C_NULL "could not open window with GLFW3."
 
-    # set callbacks
-    GLFW.SetKeyCallback(window, key_callback)
-    GLFW.SetWindowSizeCallback(window, window_size_callback)
-    GLFW.MakeContextCurrent(window)
-    GLFW.WindowHint(GLFW.SAMPLES, 4)
+	GLFW.SetKeyCallback(window, key_callback)
+	GLFW.SetWindowSizeCallback(window, window_size_callback)
+	GLFW.SetFramebufferSizeCallback(window, framebuffer_size_callback)
+	GLFW.MakeContextCurrent(window)
+	GLFW.WindowHint(GLFW.SAMPLES, 4)
 
-    # get version info
+	# get version info
     renderer = unsafe_string(glGetString(GL_RENDERER))
     version = unsafe_string(glGetString(GL_VERSION))
-    info("Renderder: ", renderer)
-    info("OpenGL version supported: ", version)
-    glparams()
+    @info "Renderder: $renderer"
+    @info "OpenGL version supported: $version"
 
-    return true
+    return window
 end
 
-# _update_fps_counter
-let previousTime = time()
-    frameCount = 0
-    global function updatefps(window::GLFW.Window)
-        currentTime = time()
-        elapsedTime = currentTime - previousTime
-        if elapsedTime > 0.25
-            previousTime = currentTime
-            fps = frameCount / elapsedTime
-            s = @sprintf "opengl @ fps: %.2f" fps
-            GLFW.SetWindowTitle(window, s)
-            frameCount = 0
-        end
-        frameCount = frameCount + 1
-    end
+# _update_fps_counter functor
+mutable struct FPSCounter{T<:AbstractFloat}
+	previous_time::T
+	frame_count::Int
+end
+FPSCounter() = FPSCounter(time(), 0)
+function (obj::FPSCounter)(window::GLFW.Window)
+	current_time = time()
+	elapsed_time = current_time - obj.previous_time
+	if elapsed_time > 0.25
+		obj.previous_time = current_time
+		fps = obj.frame_count / elapsed_time
+		GC.@preserve fps GLFW.SetWindowTitle(window, @sprintf("opengl @ fps: %.2f", fps))
+		obj.frame_count = 0
+	end
+	obj.frame_count += 1
 end
